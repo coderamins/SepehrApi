b107a104e5f46983bacb31ca5aecdd12aeaed2af
RaminS <44978175+coderamins@users.noreply.github.com>
2024-06-29 13:47:00 +0330

    202406290146pm

---
+++ src/Sepehr.Infrastructure.Persistence/Data/20240627100204_202406270129pm.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sepehr.Infrastructure.Persistence.Data
+{
+    /// <inheritdoc />
+    public partial class _202406270129pm : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrderNo",
+                schema: "sepdb",
+                table: "ApplicationMenus",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OrderNo",
+                schema: "sepdb",
+                table: "ApplicationMenus");
+        }
+    }
+}
